a7d7e86e2015d985c7a4a5061eb06d89ca200d77
Mauli Kashid <kashidnamdevmauli777@gmail.com>
2025-02-15 13:16:30 +0530

    updating s

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className={`header-container ${isDark?'dark':''}`}>
             <div className="header-content">
-                <h2 className="title"><a href="/">Where in the world?</a></h2>
+                <h2 className="title"><a href="/">Where in the worlds?</a></h2>
                 <p onClick={() => {  setIsDark(!isDark)
                     localStorage.setItem('isDarkMode',!isDark)
                 }} className="theme-changer"><i className={`fa-regular fa-${isDark ? 'sun':'moon'}`}></i>&nbsp;&nbsp;{isDark?'Light':'Dark'} Mode</p>
